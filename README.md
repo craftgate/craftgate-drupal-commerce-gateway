bce8e301531aa814fb061cf36416b6a81acd58bf
Sabri Onur Tüzün <335067+sotuzun@users.noreply.github.com>
2021-06-22 11:40:43 +0300

    adds minor fixes and readme file (#2)

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# craftgate-drupal-gateway
-The Official Craftgate Payment Gateway Module for Drupal Commerce 2.x
+# Craftgate Payment Gateway
+Craftgate Drupal Commerce 2.x modülünü kullanarak işletmenize ait tüm bankaların Sanal POSlarını tek merkezden yönetebilir, gelişmiş ödeme formu entegrasyonu ile kolayca ve güvenle ödeme alabilirsiniz.
+
+### Craftgate nedir?
+Craftgate, bulut tabanlı ödeme geçidi ile işletmenize ait tüm bankaların Sanal POSlarını tek merkezden kolayca entegre edebilir, kart saklama, tek tıkla ödeme, pazaryeri çözümü, kapalı devre cüzdan, ödeme formu, ortak ödeme sayfası, gelişmiş üye işyeri kontrol paneli, gelişmiş API gibi birçok katma değerli servisten hızlı bir şekilde yararlanabilirsiniz. Böylece, maliyetlerinizi düşürürken, siz asıl işinizi büyütmeye odaklanabilirsiniz.
+
+### Craftgate ile çalışmaya nasıl başlayabilirim?
+1. [craftgate.io](https://craftgate.io) sayfamızdan Kayıt Ol butonuna tıklayınız.
+1. Sizden istenilen bilgileri doldurup, belgeleri yükleyip, online olarak başvurunuz.
+1. Gelen dijital sözleşmeyi onaylayın ve Craftgate’i hemen kullanmaya başlayınız.
+
+### Ürün özellikleri
+* Tüm bankalarla hazır Sanal POS entegrasyonu
+* Pazaryeri, alt üye işyeri ve para dağıtma modeli desteği
+* PCI-DSS-1 uyumlu kart saklama, tek tıkla ödeme ve abonelik çözümü
+* Kapalı devre cüzdan çözümü
+* Ödeme formu, ortak ödeme sayfası
+* Akıllı ve Dinamik Ödeme Yönlendirme Kuralları
+* Ödeme Tekrar Deneme
+* Gelişmiş Üye İşyeri kontrol paneli
+* Birçok programlama dilini kapsayan, kolay entegre edilebilir API
+* 2007 yılından bu yana bir çok tecrübe ve birikimle geliştirilmiş, ölçeklenebilir, 6. nesil platform
+
+### Gereksinimler
+Craftgate kullanarak ödeme geçirebilmek için üyeliğinizin olması gerekmektedir.
+
+### Geliştiriciler
+Drupal Commerce 2.x eklentisi [Bidolubaskı](https://github.com/bidolubaski) tarafından geliştirilmiştir ve artık Craftgate tarafından bakımı yapılmaktadır.
--- src/Plugin/Commerce/PaymentGateway/Craftgate.php
+++ src/Plugin/Commerce/PaymentGateway/Craftgate.php
@@ -4,17 +4,17 @@ namespace Drupal\commerce_craftgate\Plugin\Commerce\PaymentGateway;
 
 use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\commerce_payment\Entity\PaymentInterface;
-use Drupal\commerce_price\Price;
 use Drupal\commerce_payment\Exception\PaymentGatewayException;
+use Drupal\commerce_payment\PaymentMethodTypeManager;
+use Drupal\commerce_payment\PaymentTypeManager;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\SupportsRefundsInterface;
+use Drupal\commerce_price\Price;
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\user\UserInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\commerce_payment\PaymentMethodTypeManager;
-use Drupal\commerce_payment\PaymentTypeManager;
-use Drupal\Component\Datetime\TimeInterface;
 
 /**
  * Provides the Craftgate offsite Checkout payment gateway.
@@ -40,100 +40,100 @@ use Drupal\Component\Datetime\TimeInterface;
 class Craftgate extends OffsitePaymentGatewayBase implements SupportsRefundsInterface
 {
 
-  public $craftgateClient;
+    public $craftgateClient;
 
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PaymentTypeManager $payment_type_manager, PaymentMethodTypeManager $payment_method_type_manager, TimeInterface $time)
-  {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $payment_type_manager, $payment_method_type_manager, $time);
-    $this->init();
-  }
-
-  public function __wakeup()
-  {
-    $this->init();
-  }
-
-  protected function init()
-  {
-    $url = $this->getMode() == 'test' ? 'https://sandbox-api.craftgate.io' : 'https://api.craftgate.io';
-    $this->craftgateClient = new \Craftgate\Craftgate(
-      array(
-        'apiKey' => $this->configuration['api_key'],
-        'secretKey' => $this->configuration['secret_key'],
-        'baseUrl' => $url,
-      )
-    );
-  }
-
-  public function buildConfigurationForm(array $form, FormStateInterface $form_state)
-  {
-    $form = parent::buildConfigurationForm($form, $form_state);
-    $form['api_key'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('API key'),
-      '#description' => $this->t('This is the api key from the Craftgate panel.'),
-      '#default_value' => $this->configuration['api_key'],
-      '#required' => TRUE,
-    ];
-
-    $form['secret_key'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Secret key'),
-      '#description' => $this->t('The secret key from the Craftgate panel.'),
-      '#default_value' => $this->configuration['secret_key'],
-      '#required' => TRUE,
-    ];
-
-    return $form;
-  }
-
-  public function submitConfigurationForm(array &$form, FormStateInterface $form_state)
-  {
-    parent::submitConfigurationForm($form, $form_state);
-    $values = $form_state->getValue($form['#parents']);
-    $this->configuration['api_key'] = $values['api_key'];
-    $this->configuration['secret_key'] = $values['secret_key'];
-  }
-
-  public function onReturn(OrderInterface $order, Request $request)
-  {
-    $token = $request->request->get('token');
-
-    if ($token == null) {
-      throw new PaymentGatewayException(t("Post data didn't have 'token' information."));
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PaymentTypeManager $payment_type_manager, PaymentMethodTypeManager $payment_method_type_manager, TimeInterface $time)
+    {
+        parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $payment_type_manager, $payment_method_type_manager, $time);
+        $this->init();
     }
-    $response = json_decode($this->craftgateClient->payment()->retrieveCheckoutPayment($token));
-    if ($response->errors ?? false) {
-      throw new PaymentGatewayException($response->errors->errorDescription);
-    }
-    if ($response->data->paymentStatus != 'SUCCESS') {
-      throw new PaymentGatewayException(t('Payment status of @status is not expected.', ['@status' => $response->data->paymentStatus]));
-    }
-    $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
-    $payment = $payment_storage->create([
-      'state' => 'completed',
-      'amount' => new Price($response->data->paidPrice, $response->data->currency),
-      'payment_gateway' => $this->parentEntity->id(),
-      'order_id' => $order->id(),
-      'test' => $this->getMode() == 'test',
-      'remote_id' => $response->data->id
-    ]);
-    $payment->save();
-    if ($response->data->cardUserKey ?? false) {
-      $user = $order->getCustomer();
-      $this->setRemoteCustomerId($user, $response->data->cardUserKey);
-      $user->save();
-    }
-  }
 
-  public function refundPayment(PaymentInterface $payment, ?Price $amount = NULL)
-  {
-    // Craftgate only allows refund by order item/transaction so the specified order item/transaction must be sent.
-    // In order to achive that we provide another method called refundPaymentByTransaction and perform refund there.
-    // Also PaymentRefundForm is overriden to use refundPaymentByTransaction method
-  }
+    public function __wakeup()
+    {
+        $this->init();
+    }
 
-  public function refundPaymentByTransaction(PaymentInterface $payment, $transaction)
+    protected function init()
+    {
+        $url = $this->getMode() == 'test' ? 'https://sandbox-api.craftgate.io' : 'https://api.craftgate.io';
+        $this->craftgateClient = new \Craftgate\Craftgate(
+            array(
+                'apiKey' => $this->configuration['api_key'],
+                'secretKey' => $this->configuration['secret_key'],
+                'baseUrl' => $url,
+            )
+        );
+    }
+
+    public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+    {
+        $form = parent::buildConfigurationForm($form, $form_state);
+        $form['api_key'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('API key'),
+            '#description' => $this->t('This is the api key from the Craftgate panel.'),
+            '#default_value' => $this->configuration['api_key'],
+            '#required' => TRUE,
+        ];
+
+        $form['secret_key'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Secret key'),
+            '#description' => $this->t('The secret key from the Craftgate panel.'),
+            '#default_value' => $this->configuration['secret_key'],
+            '#required' => TRUE,
+        ];
+
+        return $form;
+    }
+
+    public function submitConfigurationForm(array &$form, FormStateInterface $form_state)
+    {
+        parent::submitConfigurationForm($form, $form_state);
+        $values = $form_state->getValue($form['#parents']);
+        $this->configuration['api_key'] = $values['api_key'];
+        $this->configuration['secret_key'] = $values['secret_key'];
+    }
+
+    public function onReturn(OrderInterface $order, Request $request)
+    {
+        $token = $request->request->get('token');
+
+        if ($token == null) {
+            throw new PaymentGatewayException(t("Post data didn't have 'token' information."));
+        }
+        $response = json_decode($this->craftgateClient->payment()->retrieveCheckoutPayment($token));
+        if ($response->errors ?? false) {
+            throw new PaymentGatewayException($response->errors->errorDescription);
+        }
+        if ($response->data->paymentStatus != 'SUCCESS') {
+            throw new PaymentGatewayException(t('Payment status of @status is not expected.', ['@status' => $response->data->paymentStatus]));
+        }
+        $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
+        $payment = $payment_storage->create([
+            'state' => 'completed',
+            'amount' => new Price($response->data->paidPrice, $response->data->currency),
+            'payment_gateway' => $this->parentEntity->id(),
+            'order_id' => $order->id(),
+            'test' => $this->getMode() == 'test',
+            'remote_id' => $response->data->id
+        ]);
+        $payment->save();
+        if ($response->data->cardUserKey ?? false) {
+            $user = $order->getCustomer();
+            $this->setRemoteCustomerId($user, $response->data->cardUserKey);
+            $user->save();
+        }
+    }
+
+    public function refundPayment(PaymentInterface $payment, ?Price $amount = NULL)
+    {
+        // Craftgate only allows refund by order item/transaction so the specified order item/transaction must be sent.
+        // In order to achive that we provide another method called refundPaymentByTransaction and perform refund there.
+        // Also PaymentRefundForm is overriden to use refundPaymentByTransaction method
+    }
+
+    public function refundPaymentByTransaction(PaymentInterface $payment, $transaction)
     {
         $this->assertPaymentState($payment, ['completed', 'partially_refunded']);
         // Perform the refund request here, throw an exception if it fails.
@@ -166,8 +166,8 @@ class Craftgate extends OffsitePaymentGatewayBase implements SupportsRefundsInte
         $payment->save();
     }
 
-  public function getRemoteCustomerId(UserInterface $account)
-  {
-    return parent::getRemoteCustomerId($account);
-  }
+    public function getRemoteCustomerId(UserInterface $account)
+    {
+        return parent::getRemoteCustomerId($account);
+    }
 }
